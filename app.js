19e98e56b6d946d8d075aebbe298a671ba731f6b
mapuebla <mariopuebla@gmail.com>
2019-09-24 13:15:07 -0300

    Seccion 11 terminada - CRUD - Imagenes

--- app.js
+++ app.js
@@ -15,15 +15,37 @@ app.use(bodyParser.json());
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 // Conexión a la Base de Datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+mongoose.set('useCreateIndex', true)
+mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true }, (err, res) => {
     if (err) throw err;
     console.log('Base de Datos: \x1b[32m%s\x1b[0m', ' online');
 });
 
+/*mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+    console.log('Base de Datos: \x1b[32m%s\x1b[0m', ' online');
+});*/
+
+// Serve index config
+/* var serveIndex = require('serve-index');
+app.use(express.static(__dirname + '/'))
+app.use('/uploads', serveIndex(__dirname + '/uploads'));
+ */
+
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+Utilizar la librería express-fileupload
+url:
+    https://github.com/richardgirges/express-fileupload
+instalación:
+    npm i express-fileupload
+utilización:
+    var fileUpload = required('express-fileupload');
+    
